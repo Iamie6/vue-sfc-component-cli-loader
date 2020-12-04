7a2cf87c443987fac08f0a1724f9dddbbf73196f
mengdehua <mengdehuavipkid.com.cn@igetter.local>
2020-12-04 15:22:59 +0800

    fix 仓库地址获取

--- utils/repository.js
+++ utils/repository.js
@@ -1,16 +1,19 @@
 const execSync = require('child_process').execSync
-const branchCommand = 'git remote -v'
-const remote = execSync(branchCommand).toString()
+const repositoryCommand = 'git remote -v'
+const userCommand = 'git config user.name'
 const chalk = require('chalk')
-const repositoryName = remote.match(/\.com\.cn(:\d+)?\/(.*)\.git/)
 
 module.exports = function () {
-  if (!repositoryName) {
-    console.log(chalk.red('本项目没有关联远程仓库'))
-  }
-  if (repositoryName.length>1) {
+  const remote = execSync(repositoryCommand).toString()
+  const user = execSync(userCommand).toString()
+  const repositoryName = remote.match(/\.com\.cn(:\d+)?\/(.*)\.git/)
+  if (repositoryName && repositoryName.length>1) {
     return repositoryName[2]
   } else {
-    return '@mit'
+    if (!user) {
+      console.log(chalk.red('目前的仓库需要关联远程仓库。 \n 或者通过git config --global user.name "username" 添加用户名'))
+      process.exit(1)
+    }
+    return `mit/${user}`
   }
 }
