db34442d87f9269dd704fdce46c37b9bfe8c412f
mengdehua <mengdehuavipkid.com.cn@igetter.local>
2020-12-04 14:22:13 +0800

    增加上传日志 0.1.3

---
+++ utils/log.js
@@ -0,0 +1,16 @@
+const log4js = require('log4js')
+log4js.configure({
+  appenders:{ 
+    "upload-res": { type: 'file', filename: 'build.log' }
+  },
+  categories: {
+    default: {
+      appenders: ['upload-res'],
+      level: 'info'
+    }
+  }
+})
+
+module.exports = function (type) {
+  return log4js.getLogger(type)
+}
